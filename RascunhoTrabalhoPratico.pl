d210fbe85cf8f4ef6b4ff8d10fd4d3ce1308c210
delgas12 <jpdelgado2001@gmail.com>
2021-11-28 00:22:08 +0000

    Algumas alterações

--- RascunhoTrabalhoPratico.pl
+++ RascunhoTrabalhoPratico.pl
@@ -9,30 +9,30 @@ veiculo(carro,3).
 
 %encomenda(Id,Peso,Volume,Classificacao,Rua,Freguesia,Veiculo,Preço,idCliente,idEstafeta).
 
-encomenda(1 ,5  ,8  ,4 ,"Rua Santo António, nº420"          ,"Celeirós"      ,bicicleta ,10 ,123   ,987).
-encomenda(2 ,14 ,10 ,5 ,"Praça do Comércio, nº15"           , "Silveiros"    ,mota      ,9  ,456   ,654).
-encomenda(3 ,90 ,5  ,3 ,"Entrocamento de São Geraldes, nº30","Martim"        ,carro     ,14 ,789   ,321).
-encomenda(4 ,6  ,6  ,4 ,"Rua José Sócrates, nº2"            ,"Fraião"        ,carro     ,15 ,12345 ,987).
-encomenda(5 ,4  ,13 ,4 ,"Rua Monte Carrinhos , nº45"        ,"Martim"        ,mota      ,15 ,123   ,654).
-encomenda(6 ,12 ,9  ,3 ,"Rua Paulo Fernandes , nº48"        ,"Santa Eugénia" ,carro     ,20 ,456   ,321).
-encomenda(7 ,3  ,4  ,2 ,"Praça Arsenalistas  , nº410"       ,"Cabreiros"     ,bicicleta ,50 ,12345 ,321).
-encomenda(8 ,10 ,12 ,4 ,"Rua do Souto , nº13"               ,"Sao Vitor"     ,mota      ,23 ,12345 ,654).
-encomenda(9 ,9  ,3  ,2 ,"Rua Santa Margarida ,nº60"         ,"Vermil"        ,carro     ,2  ,123   ,654).
+encomenda(1, 5 , 8 , 4, "Rua Santo António, nº420"          , "Celeirós"     , bicicleta, 10, 123  , 987).
+encomenda(2, 14, 10, 5, "Praça do Comércio, nº15"           ,  "Silveiros"   , mota     , 9 , 456  , 654).
+encomenda(3, 90, 5 , 3, "Entrocamento de São Geraldes, nº30", "Martim"       , carro    , 14, 789  , 321).
+encomenda(4, 6 , 6 , 4, "Rua José Sócrates, nº2"            , "Fraião"       , carro    , 15, 12345, 987).
+encomenda(5, 4 , 13, 4, "Rua Monte Carrinhos , nº45"        , "Martim"       , mota     , 15, 123  , 654).
+encomenda(6, 12, 9 , 3, "Rua Paulo Fernandes , nº48"        , "Santa Eugénia", carro    , 20, 456  , 321).
+encomenda(7, 3 , 4 , 2, "Praça Arsenalistas  , nº410"       , "Cabreiros"    , bicicleta, 50, 12345, 321).
+encomenda(8, 10, 12, 4, "Rua do Souto , nº13"               , "Sao Vitor"    , mota     , 23, 12345, 654).
+encomenda(9, 9 , 3 , 2, "Rua Santa Margarida ,nº60"         , "Vermil"       , carro    , 2 , 123  , 654).
 
 %dataE(IdEnc ,DataEnc, Prazo, DataEnt).
 
-dataE(1,20/11/21 ,21/11/21 ,21/11/21 ).
-dataE(2,20/11/21 ,23/11/21 ,22/11/21 ).
-dataE(3,25/12/21 ,31/12/21 ,01/01/22 ).
-dataE(4,31/12/21 ,01/01/22 ,01/01/22 ).
-dataE(5,27/03/21 ,28/03/21 ,28/03/21 ).
-dataE(6,09/12/19 ,10/12/19 ,11/12/19 ).
-dataE(7,03/07/18 ,15/07/18 ,18/07/18 ).
+dataE(1, 20/11/21, 21/11/21, 21/11/21 ).
+dataE(2, 20/11/21, 23/11/21, 22/11/21 ).
+dataE(3, 25/12/21, 31/12/21, 01/01/22 ).
+dataE(4, 31/12/21, 01/01/22, 01/01/22 ).
+dataE(5, 27/03/21, 28/03/21, 28/03/21 ).
+dataE(6, 09/12/19, 10/12/19, 11/12/19 ).
+dataE(7, 03/07/18, 15/07/18, 18/07/18 ).
 
 %dataNE(IdEnc, DataEnc, Prazo).
 
-dataNE(8 ,20/11/21 ,21/11/21).
-dataNE(9 ,20/11/21 ,23/11/21).
+dataNE(8, 20/11/21, 21/11/21).
+dataNE(9, 20/11/21, 23/11/21).
 
 
 %encomenda(10, 3 , 2 , "Largo Sr. dos Aflitos, nº4")
@@ -43,16 +43,16 @@ dataNE(9 ,20/11/21 ,23/11/21).
 %cliente(Id,IdsEncomendas).
 %IdsEncomendas -> lista dos ids
 
-cliente(123   ,[1,5]).
-cliente(12345 ,[4,7]).
-cliente(456   ,[2,6]).
-cliente(789   ,[3]).
+cliente(123  , [1,5]).
+cliente(12345, [4,7]).
+cliente(456  , [2,6]).
+cliente(789  , [3]).
 
 
-%estafeta(Id, NumEnc,Rank,Encomendas,NumPen).
-estafeta(987 ,2 ,5 ,[1,4]   ,0).
-estafeta(654 ,2 ,3 ,[2,5]   ,3).
-estafeta(321 ,3 ,4 ,[3,6,7] ,2).
+%estafeta(Id, NumEnc,Encomendas,NumPen).
+estafeta(987, 2, [1,4]  , 0).
+estafeta(654, 2, [2,5]  , 3).
+estafeta(321, 3, [3,6,7], 2).
 
 
 %-------------------------- QUERY 1 --------------------------------------------
@@ -67,9 +67,9 @@ maxEcologico(Result) :- encomendasEstafeta(Pares),maxEcologicoAux(Pares,Result).
 
 maxEcologicoAux([(Estafeta,_)],Estafeta):-!.
 maxEcologicoAux([E1,E2|T],Result):- media(E1,P1),
-                                    media(E2,P2), 
+                                    media(E2,P2),
                                     P1 =< P2 ,
-                                    !, 
+                                    !,
                                     maxEcologicoAux([E1|T],Result).
 maxEcologicoAux([_|T],Result):- maxEcologicoAux(T,Result).
 
@@ -87,13 +87,11 @@ pontuacao((_,[(_,_,_,_,_,_,carro,_,_,_)|Y]),R):- pontuacao((_,Y),R1), R is R1 + 
 
 
 estafetasCliente(_,[],[]).
-estafetasCliente(Cliente,[X|T],[E|Result]):- 
+estafetasCliente(Cliente,[X|T],[E|Result]):-
                         estafetasClienteAux(Cliente,X,E),
                         estafetasCliente(Cliente,T,Result),
                         \+member(E,Result).
 
-
-%encomenda(Id,Peso,Volume,Classificacao,Rua,Freguesia,Veiculo,Preço,idCliente,idEstafeta).
 
 
 estafetasClienteAux(Cliente,X,E):-encomenda(X,_,_,_,_,_,_,_,Cliente,E).
@@ -120,17 +118,11 @@ clientesEstafeta(Estafeta,R):- encomendasEstafeta(Pares),
 %-------------------------- QUERY 4 --------------------------------------------
 %calcular o valor faturado pela Green Distribution num determinado dia;
 
-%encomenda(Id,Peso,Volume,Classificacao,Rua,Freguesia,Veiculo,Preço,idCliente,idEstafeta).
-%datasE(IdEnc ,DataEnc, Prazo, DataEnt).
-%datasNE(IdEnc, DataEnc, Prazo).
-
 valorDiario(Data,Result):- findall((Preco) , (encomenda(Id,_,_,_,_,_,_,Preco,_,_) , dataE(Id,Data,_,_) ), LE ),
                            findall((Preco) , (encomenda(Id,_,_,_,_,_,_,Preco,_,_) , dataNE(Id,Data,_) ), LNE ),
                            append(LE,LNE,L),
-                           somatorio(L,Result).
+                           sum_list(L,Result).
 
-somatorio([],0).
-somatorio([X|T],Result):- somatorio(T,R), Result is R+X.
 
 
 
@@ -138,18 +130,11 @@ somatorio([X|T],Result):- somatorio(T,R), Result is R+X.
 
 %identificar quais as zonas (e.g., rua ou freguesia) com maior volume deentregas por parte da Green Distribution;
 
-%encomenda(Id,Peso,Volume,Classificacao,Rua,Freguesia,Veiculo,Preço,idCliente,idEstafeta).
-
-maisEntregasFreg(Result):- 
-                findall((Freguesia) , encomenda(_,_,_,_,_,Freguesia,_,_,_,_),Freguesias), 
+maisEntregasFreg(Result):-
+                findall((Freguesia) , encomenda(_,_,_,_,_,Freguesia,_,_,_,_),Freguesias),
                 difsList2(Freguesias,List),
                 maisEntregasAux(List,Pares),
                 maiorEntregas(Pares,Result).
-
-
-difsList([],P,R):- length(P,R),!.
-difsList([H|T],P,R):- member(H,P),difsList(T,P,R).
-difsList([H|T],P,R):- not(member(H,P)),append([P,[H]],N),difsList(T,N,R).
 
 
 difsList2([],[]):-!.
@@ -157,8 +142,8 @@ difsList2([X|T],[X|Result]):- difsList2(T,Result),\+member(X,Result),!.
 difsList2([X|T],Result):-  difsList2(T,Result).
 
 maisEntregasAux([],[]).
-maisEntregasAux([Freguesia|T] , [(Freguesia,Reps)|Result]) :-findall((Freguesia) , encomenda(_,_,_,_,_,Freguesia,_,_,_,_), ListaX), 
-                                             length(ListaX,Reps), 
+maisEntregasAux([Freguesia|T] , [(Freguesia,Reps)|Result]) :-findall((Freguesia) , encomenda(_,_,_,_,_,Freguesia,_,_,_,_), ListaX),
+                                             length(ListaX,Reps),
                                              maisEntregasAux(T,Result).
 
 maiorEntregas([(F,_)],F):-!.
@@ -169,12 +154,10 @@ maiorEntregas([_|T],Result):- maiorEntregas(T,Result).
 
 %-------------------------- QUERY 6 --------------------------------------------
 
-classMedia(IdEstafeta , Result):- encomendasEstafeta(ListEncomendas), 
+classMedia(IdEstafeta , Result):- encomendasEstafeta(ListEncomendas),
                                     filtrarEstafeta(IdEstafeta, ListEncomendas, Encomendas),
                                     mediaC(Encomendas,0,0, Result).
 
-
-%encomenda(Id,Peso,Volume,Classificacao,Rua,Freguesia,Veiculo,Preço,idCliente,idEstafeta).
 
 
 
@@ -195,8 +178,8 @@ entregasPeriodoTransporte(DataI,DataF, Result):- findall((Transporte), (encomend
 
 contaTransporte([], 0/0/0).
 contaTransporte([bicicleta|T],AccB/AccM/AccC):- contaTransporte(T,Acc1/AccM/AccC), AccB is Acc1+1.
-contaTransporte([mota|T],AccB/AccM/AccC):- contaTransporte(T,AccB/Acc2/AccC), AccM is Acc2+1. 
-contaTransporte([carro|T],AccB/AccM/AccC):- contaTransporte(T,AccB/AccM/Acc3), AccC is Acc3+1. 
+contaTransporte([mota|T],AccB/AccM/AccC):- contaTransporte(T,AccB/Acc2/AccC), AccM is Acc2+1.
+contaTransporte([carro|T],AccB/AccM/AccC):- contaTransporte(T,AccB/AccM/Acc3), AccC is Acc3+1.
 
 
 
@@ -214,7 +197,7 @@ antes(D1/M1/A1,DI/MI/AI):- A1<AI , ! ;
 
 
 entregasPeriodo(DataI,DataF, Result):- findall((Id), (encomenda(Id,_,_,_,_,_,_,_,_,_), dataE(Id,_,_,DataE), depois(DataE,DataI), antes(DataE,DataF)) ,L),
-                                       length(L,Result).  
+                                       length(L,Result).
 
 
 %-------------------------- QUERY 9 --------------------------------------------
@@ -223,13 +206,11 @@ entregasPeriodo(DataI,DataF, Result):- findall((Id), (encomenda(Id,_,_,_,_,_,_,_
 encomendasPeriodo(DataI,DataF, E/NE):- findall((Id), (encomenda(Id,_,_,_,_,_,_,_,_,_), dataE(Id,Data,_,_), depois(Data,DataI), antes(Data,DataF)) ,LE),
                                        findall((Id), (encomenda(Id,_,_,_,_,_,_,_,_,_), dataNE(Id,Data,_), depois(Data,DataI), antes(Data,DataF)) ,LNE),
                                        length(LE,E),
-                                       length(LNE,NE).  
+                                       length(LNE,NE).
 
 
 %-------------------------- QUERY 10 --------------------------------------------
 %calcular o peso total transportado por estafeta num determinado dia;
-
-%encomenda(Id,Peso,Volume,Classificacao,Rua,Freguesia,Veiculo,Preço,idCliente,idEstafeta).
 
 
 pesoEstafetaDia(Id,Dia,Result) :- findall( (Peso) , (encomenda(IdE,Peso,_,_,_,_,_,_,_,Id) , dataE(IdE,_,_,Dia) ) , L),
@@ -245,9 +226,8 @@ pesoEstafetaDia(Id,Dia,Result) :- findall( (Peso) , (encomenda(IdE,Peso,_,_,_,_,
 
 %---------cria a lista de pares (Estafeta,[Encomendas])------------
 encomendasEstafeta(ListEncomendas):-
-    findall((Estafeta, ListaNumEncomendas) , estafeta(Estafeta, _, _, ListaNumEncomendas, _) ,L),
+    findall((Estafeta, ListaNumEncomendas) , estafeta(Estafeta, _, ListaNumEncomendas, _) ,L),
     listEncomendas(ListEncomendas,L).
-    
 
 
 %--- substitui a lista de numeros de encomenda pela lista de encomendas
@@ -261,10 +241,3 @@ listEncomendasAux([],[]).
 listEncomendasAux([(NE,A,B,C,D,E,F,G,H,I,J,K) | LE] , [NE | LNE] ):-
     encomenda(NE,A,B,C,D,E,F,G,H,I,J,K),
     listEncomendasAux(LE,LNE).
-
-
-
-
-
-
-
